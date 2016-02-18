588addec8f7a59ad59238d19eca6f4f0e40d83cc
Ryan Thompson <service@aiwebsystems.com>
2016-02-18 03:24:42 -0600

    Field type attributes

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,7 +1,7 @@
 $(function () {
 
     // Initialize colorpickers
-    $('.colorpicker-field_type .colorpicker-component').each(function () {
-        $(this).colorpicker();
+    $('input[data-provides="anomaly.field_type.colorpicker"]').each(function () {
+        $(this).closest('.colorpicker-component').colorpicker();
     });
 });
