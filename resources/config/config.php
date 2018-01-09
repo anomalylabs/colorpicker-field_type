6461a7bc217e349461a89fb28a4676a4525858ea
Ryan Thompson <ryan@pyrocms.com>
2018-01-09 08:40:48 -0600

    Docs

--- resources/config/config.php
+++ resources/config/config.php
@@ -1,9 +1,6 @@
 <?php
 
 return [
-    'colors'        => [
-        'type' => 'anomaly.field_type.textarea',
-    ],
     'default_value' => [
         'type'   => 'anomaly.field_type.colorpicker',
         'config' => [
--- resources/lang/en/config.php
+++ resources/lang/en/config.php
@@ -5,9 +5,4 @@ return [
         'label'        => 'Default Value',
         'instructions' => 'Choose a default color if any.',
     ],
-    'colors'        => [
-        'label'        => 'Predefined Colors',
-        'instructions' => 'Enter each color value <strong>(hex, rgb, or rgba)</strong> on a new line.',
-        'warning'      => 'If predefined colors are not in the required format they will be converted when selected.',
-    ],
 ];
